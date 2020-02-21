e5b3254c9fad4559522a821bf6a8e9b09b418f8b
Josué Nicholson <nicholsonjosue@alu.ufc.br>
2020-02-21 14:47:43 -0300

    Update contact implementado

--- src/main/java/com/contactbook/controller/ContactController.java
+++ src/main/java/com/contactbook/controller/ContactController.java
@@ -1,13 +1,13 @@
 package com.contactbook.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +45,11 @@ public class ContactController {
 	@DeleteMapping("/contact/{id}")
 	public void deleteContact(@PathVariable(value="id") long id) {
 		 contactRepository.deleteById(id);
+	}
+	
+	@PutMapping("/contact")
+	public Contact updateContact(@RequestBody Contact contact){
+		return contactRepository.save(contact);
 	}
 	
 
